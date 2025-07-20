97b8bbce2c663727a3da18efa9fb5eecc6645229
Ari Cummings <ari@aricummings.com>
2025-07-20 00:23:47 -0400

    d

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "192.168.88.3",
     port: 8080,
-    
+    cors: {
+      origin: "https://aricummings.com", // Allow access from this origin
+      methods: ['GET', 'POST'], // Specify allowed methods
+      allowedHeaders: ['Content-Type'], // Specify allowed headers
+    },
   },
   plugins: [
     react(),
