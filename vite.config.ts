b084d8a8931cd42df3054e426b1edca84b46320a
Ari Cummings <ari@aricummings.com>
2025-07-20 00:01:06 -0400

    d

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "aricummings.com",
-    port: 80,
+    host: "::",
+    port: 8080,
   },
   plugins: [
     react(),
